ea48899af9952115078a6ca45e417a8aa47e29f2
Davi DeBarros <ddebarros@digitalocean.com>
2022-08-28 14:34:27 -0400

    Changes the project to support common TS modules

--- packages/sample/hello/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# When deploying, the doctl CLI will run this script to build the  
-# necessary accests required.
-yarn install 
-yarn build
-
-rm -rf node_modules
-yarn install --production --ignore-engines
